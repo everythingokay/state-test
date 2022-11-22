8ee62385d69691bd1594e1140dfc5384c201bf52
Alison Feldish <alison.feldish@gmail.com>
2022-11-22 17:56:14 -0500

    constructor function

---
+++ src/Clicker.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Clicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { num: 1 };
+        this.generateRandom = this.generateRandom.bind(this);
+    }
+    generateRandom() {
+        let random = Math.floor(Math.random() * 10) + 1;
+        this.setState({num: random});
+    }
+    render() {
+        return (
+            <div>
+                <h2>Number is:</h2>
+                <h1> {this.state.num}</h1>
+                <button onClick={this.generateRandom}>Generate Random Number</button>
+            </div>
+        )
+    }
+}
+
+export default Clicker;
